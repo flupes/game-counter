78fbb1dea2b954a583b978a51dc5aff2312fd58e
Lorenzo Flueckiger <lorenzo@flupes.xyz>
2022-04-19 20:05:51 -0700

    try connectivity with basic loop

--- app/monitor.py
+++ app/monitor.py
@@ -8,39 +8,41 @@ dev.port = 'COM8'
 dev.baudrate = 9600
 dev.write_timeout = 3
 
+RECONNECT_WAIT = 5
+UPDATE_PERIOD = 2
+
+# From: https://stackoverflow.com/questions/12435211/threading-timer-repeat-function-every-n-seconds
 class RepeatTimer(Timer):
     def run(self):
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
 
-def update(dev):
-  update.counter += 1
+stopped = False
+numtries = 0
+while (not stopped):
   try:
-    dev.write(bytes([update.counter]))
-    print('wrote', update.counter)
-  except serial.SerialTimeoutException as timeout:
-    print(timeout)
-
-update.counter = 0
-
-def connect(dev):
-  connect.numtries += 1
-  print('trying to open serial port #', connect.numtries)
-  updater = None
-  try:
+    numtries += 1
+    print('trying to open serial port #', numtries)
     dev.open()
-    updater = RepeatTimer(2, update(dev))
-    updater.start()
+    connected = True
+    updates = 0
+
+    while (connected):
+      updates += 1
+      try:
+        dev.write(bytes([updates]))
+        print('wrote', updates)
+        time.sleep(UPDATE_PERIOD)
+      except serial.SerialTimeoutException as timeout:
+        print(timeout)
+        connected = False
+      if updates > 5:
+        connected = False
+        stopped = True
+    dev.close()
   except serial.SerialException as error:
     print(error)
-    dev.close()
-    if updater:
-      updater.cancel()
-
-connect.numtries = 0
-
-opener = RepeatTimer(5, connect(dev))
-opener.start()
+  time.sleep(RECONNECT_WAIT)
 
 dev.close()
 
