5138cbe3fd623bdd3b80ab959a61224807166a3a
Lorenzo Flueckiger <lorenzo@flupes.xyz>
2022-04-19 21:47:20 -0700

    added process monitor

--- app/monitor.py
+++ app/monitor.py
@@ -1,43 +1,86 @@
 import time
 import serial
+import psutil
+import os
 
 dev = serial.Serial()
 dev.port = 'COM8'
 dev.baudrate = 9600
 dev.write_timeout = 3
 
-RECONNECT_WAIT = 5
-UPDATE_PERIOD = 2
+DEBUG = True
+
+RECONNECT_WAIT = 10
+UPDATE_PERIOD = 5
+PROCESSES_MONITORED = {
+  1: 'wsl.exe',
+  2: 'firefox.exe',
+  3: 'msedge.exe',
+  4: 'explorer.exe',
+  5: 'code.exe'
+}
+
+myusername = os.environ.get('COMPUTERNAME')+'\\'+os.environ.get('USERNAME')
+if len(PROCESSES_MONITORED) > 6:
+  print('cannot monitor more than 6 processes')
+  exit(1)
+
+def print_status(byte):
+  str = '| '
+  for k, v in PROCESSES_MONITORED.items():
+    str = str + v
+    if byte & 2**k > 0:
+      str = str + ': RUNNING'
+    else:
+      str = str + ': INACTIVE'
+    str = str + ' | '
+  print(str)
+
+def get_process_status():
+  status = 8 * [0]
+  for p in psutil.process_iter(['name', 'status', 'username']):
+    if p.info['status'] == psutil.STATUS_RUNNING and p.info['username'] == myusername:
+      for k, v in PROCESSES_MONITORED.items():
+        if status[k] == 0:
+          if p.info['name'].lower() == v:
+            status[k] = 1
+  byte = 0
+  for i in range(1, 8):
+    if status[i] == 1:
+      byte += 2**i
+  return byte
 
 stopped = False
 numtries = 0
 while (not stopped):
   try:
     numtries += 1
-    if dev.is_open:
-      print('serial port already opened')
-    else:
-      print('trying to open serial port #', numtries)
-      dev.open()
+    print('trying to open serial port #', numtries)
+    if not DEBUG:
+      if dev.is_open:
+        print('serial port already opened')
+      else:
+        dev.open()
     connected = True
     updates = 0
-
     while (connected):
       updates += 1
-      dev.write(bytes([updates]))
-      print('wrote', updates)
+      status = get_process_status()
+      if DEBUG:
+        print_status(status)
+      else:
+        dev.write(bytes([status]))
+      print('wrote', status)
       time.sleep(UPDATE_PERIOD)
-      if updates > 5:
+      if updates > 12:
         connected = False
         stopped = True
   except serial.SerialTimeoutException as timeout:
     print(timeout)
   except serial.SerialException as error:
     print(error)
-  dev.close()
+  if dev:
+    dev.close()
   time.sleep(RECONNECT_WAIT)
 
 dev.close()
-
-#un = os.environ.get('COMPUTERNAME')+'\\'+os.environ.get('USERNAME')   
-#pp([(p.pid, p.info['name']) for p in psutil.process_iter(['name', 'username', 'status']) if ( p.info['username'] == un and p.info['status'] == psutil.STATUS_RUNNING)])
