23810777057c41cacde6504c881b8f076e242cea
Lorenzo Flueckiger <lorenzo@flupes.xyz>
2022-04-19 19:46:35 -0700

    trying a threading timer: does not work with Exceptions

--- app/monitor.py
+++ app/monitor.py
@@ -1,23 +1,48 @@
 import time
 import serial
 
+from threading import Timer
+
 dev = serial.Serial()
 dev.port = 'COM8'
 dev.baudrate = 9600
 dev.write_timeout = 3
 
-try:
-  print('opening serial port')
-  dev.open()
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
-  for i in range(1, 5):
-    time.sleep(5)
-    dev.write(bytes([i]))
-    print('wrote', i)
+def update(dev):
+  update.counter += 1
+  try:
+    dev.write(bytes([update.counter]))
+    print('wrote', update.counter)
+  except serial.SerialTimeoutException as timeout:
+    print(timeout)
 
-except serial.SerialTimeoutException as timeout:
-  print(timeout)
-except serial.SerialException as error:
-  print(error)
+update.counter = 0
+
+def connect(dev):
+  connect.numtries += 1
+  print('trying to open serial port #', connect.numtries)
+  updater = None
+  try:
+    dev.open()
+    updater = RepeatTimer(2, update(dev))
+    updater.start()
+  except serial.SerialException as error:
+    print(error)
+    dev.close()
+    if updater:
+      updater.cancel()
+
+connect.numtries = 0
+
+opener = RepeatTimer(5, connect(dev))
+opener.start()
 
 dev.close()
+
+#un = os.environ.get('COMPUTERNAME')+'\\'+os.environ.get('USERNAME')   
+#pp([(p.pid, p.info['name']) for p in psutil.process_iter(['name', 'username', 'status']) if ( p.info['username'] == un and p.info['status'] == psutil.STATUS_RUNNING)])
